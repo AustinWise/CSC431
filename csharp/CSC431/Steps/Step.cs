85f82e3a30cae728dbae62db5fd477895863fdd2
Austin Wise <AustinWise@gmail.com>
2011-02-16 20:58:31 -0800

    Added a multithreaded DoAll.

--- csharp/CSC431/Steps/Step.cs
+++ csharp/CSC431/Steps/Step.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CSC431.Steps
 {
@@ -66,6 +67,64 @@ namespace CSC431.Steps
                         throw new NotSupportedException("non-output steps should not be followed");
                 }
             }
+        }
+
+        public static void DoAllThreaded(Step aStep)
+        {
+            while (aStep.parent != null)
+            {
+                aStep = aStep.parent;
+            }
+
+            Queue<Step> toDos = new Queue<Step>();
+            toDos.Enqueue(aStep);
+
+            Dictionary<Step, Task> map = new Dictionary<Step, Task>();
+            List<Task> leafTasks = new List<Task>();
+            Task top = null;
+
+            while (toDos.Count != 0)
+            {
+                Step s = toDos.Dequeue();
+
+                Task t;
+                if (s.parent == null)
+                    t = top = new Task(() => { Console.WriteLine("top"); s.Process(); });
+                else
+                    t = map[s.parent].ContinueWith(_ => s.Process());
+
+                if (isOut(s.GetType()))
+                {
+                    t = t.ContinueWith(_ =>
+                    {
+                        //it ~should~ be ok to use dynamic like this as the 
+                        //FollowWith functions should have ensured that types lined up
+                        dynamic ds = s;
+                        dynamic res = ds.Output;
+                        foreach (var n in s.nexts)
+                        {
+                            dynamic dn = n;
+                            dn.Input = res;
+                        }
+                    });
+
+                    foreach (var n in s.nexts)
+                    {
+                        toDos.Enqueue(n);
+                    }
+                }
+                else
+                {
+                    if (s.nexts.Count != 0)
+                        throw new NotSupportedException("non-output steps should not be followed");
+                    leafTasks.Add(t);
+                }
+
+                map[s] = t;
+            }
+
+            top.Start();
+            Task.WaitAll(leafTasks.ToArray());
         }
     }
 }
