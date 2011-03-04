511808c26afc78555e12da48927bb4840ce24ac8
Austin Wise <AustinWise@gmail.com>
2011-03-04 02:00:12 -0800

    Some dataflow stuff.

---
+++ csharp/CSC431/Analysis/IterativeDataflow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CSC431.CFG;
+
+namespace CSC431.Analysis
+{
+    public abstract class IterativeDataflow<T> where T : Instruction
+    {
+        protected abstract void CalculateLocal(FunctionBlock<T> fun, BasicBlock<T> block);
+        protected abstract bool Propagate(FunctionBlock<T> fun, BasicBlock<T> block);
+
+        protected void DoDataflow(ProgramBlock<T> prog)
+        {
+            foreach (var f in prog.Functions)
+            {
+                f.VisitBlocks(b =>
+                {
+                    CalculateLocal(f, b);
+                });
+                bool moreWorkNeeded;
+                do
+                {
+                    moreWorkNeeded = false;
+                    f.VisitBlocks(b =>
+                    {
+                        moreWorkNeeded |= Propagate(f, b);
+                    });
+                } while (moreWorkNeeded);
+            }
+        }
+    }
+}
---
+++ csharp/CSC431/Analysis/ReachingDef.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSC431.Analysis
+{
+    public class ReachingDef
+    {
+    }
+}
